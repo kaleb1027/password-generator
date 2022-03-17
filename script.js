d54aec5a6fee435e8cbf7d3a2ccfdd3ef0a8a848
Kaleb Sherrill <kaleb1027@gmail.com>
2022-03-17 17:28:19 -0400

    Final touch-ups and added comments to js file

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
-
-
-
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
+  //length specifications
   var pwLen = window.prompt("Enter the desired length of the password (8 - 128 characters long).")
   if(pwLen < 8 || pwLen > 128){
     window.alert("Try again, the length must be between 8 - 128 characters long (inclusive).");
     generatePassword();
   }
 
-  var lower = window.confirm("Would you like to include lowercase characters in the randomization?");
-  var upper = window.confirm("Would you like to include uppercase characters in the randomization?");
-  var number = window.confirm("Would you like to include numbers in the randomization?");
-  var special = window.confirm("Would you like to include special characters in the randomization?");
+  //character specifications
+  var lower = window.confirm("Would you like to include lowercase characters in the randomization? (\"OK\" for yes, \"Cancel\" for no)");
+  var upper = window.confirm("Would you like to include uppercase characters in the randomization? (\"OK\" for yes, \"Cancel\" for no)");
+  var number = window.confirm("Would you like to include numbers in the randomization? (\"OK\" for yes, \"Cancel\" for no)");
+  var special = window.confirm("Would you like to include special characters in the randomization? (\"OK\" for yes, \"Cancel\" for no)");
 
   if(lower == false && upper == false && number == false && special == false){
     window.alert("Sorry, you need to choose at LEAST one character type.");
     generatePassword();
   }
 
+
+  //applying character specifications
   function genLogic(){
     var charChoice = [];
     if(lower == true){
@@ -75,15 +59,13 @@ function generatePassword(){
   
   var newPW = "";
 
+  //randomly generating password
   for (var i = 0; i < pwLen; i++){
     var randomNumber = Math.floor(Math.random() * charOptions.length);
     newPW += charOptions[randomNumber];
   }
 
   return(newPW);
-  
-
-
 }
 
 // Write password to the #password input
